3343aee320bb3eb50792cda543022072d29bc062
venajeremy <venajeremy@gmail.com>
2025-05-03 00:08:40 -0700

    Development Dependency Auto Reload

--- StudentPortal/src/main/resources/static/session.js
+++ StudentPortal/src/main/resources/static/session.js
@@ -1,6 +1,9 @@
+var session = null;
 function getSession(){
     fetch("/session",
         {
             method: "GET",
-        }).then((response) => response.json()).then((json) => {return(json);});
+        }).then((response) => response.json()).then((json) => {
+            session = JSON.parse(myJSON);
+        });
 }
